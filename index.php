690797275e8d6bd59887e81d3d2b982baaab9142
Jugal Kishore <49350241+crazyuploader@users.noreply.github.com>
2019-10-31 14:19:43 +0530

    Update index.php
    sign in modal removed

--- index.php
+++ index.php
@@ -32,49 +32,6 @@ var b = document.forms["form"]["cpassword"].value;if (a!=b){alert("Passwords mus
 <span class="logo">Brain-O-Quiz</span></div>
 <div class="col-md-2 col-md-offset-4">
 <a href="#" class="pull-right btn sub1" data-toggle="modal" data-target="#myModal"><span class="glyphicon glyphicon-log-in" aria-hidden="true"></span>&nbsp;<span class="title1"><b>Signin</b></span></a></div>
-<!--sign in modal start-->
-<div class="modal fade" id="myModal">
-  <div class="modal-dialog">
-    <div class="modal-content title1">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title title1"><span style="color:orange">Log In</span></h4>
-      </div>
-      <div class="modal-body">
-        <form class="form-horizontal" action="login.php?q=index.php" method="POST">
-<fieldset>
-
-
-<!-- Text input-->
-<div class="form-group">
-  <label class="col-md-3 control-label" for="email"></label>
-  <div class="col-md-6">
-  <input id="email" name="email" placeholder="Enter your email-id" class="form-control input-md" type="email">
-
-  </div>
-</div>
-
-
-<!-- Password input-->
-<div class="form-group">
-  <label class="col-md-3 control-label" for="password"></label>
-  <div class="col-md-6">
-    <input id="password" name="password" placeholder="Enter your Password" class="form-control input-md" type="password">
-
-  </div>
-</div>
-
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        <button type="submit" class="btn btn-primary">Log in</button>
-		</fieldset>
-</form>
-      </div>
-    </div><!-- /.modal-content -->
-  </div><!-- /.modal-dialog -->
-</div><!-- /.modal -->
-<!--sign in modal closed-->
 
 </div><!--header row closed-->
 </div>
