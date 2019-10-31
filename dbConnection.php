c38e3ad833bb9e01d4a83cabd4928dfe870817ff
Jugal Kishore <49350241+crazyuploader@users.noreply.github.com>
2019-11-01 01:00:03 +0530

    Update dbConnection.php

--- dbConnection.php
+++ dbConnection.php
@@ -1,5 +1,5 @@
 <?php
 //all the variables defined here are accessible in all the files that include this one
-$con= new mysqli('localhost','root','','project')or die("Could not connect to mysql".mysqli_error($con));
+$con= new mysqli('ec2-54-217-225-16.eu-west-1.compute.amazonaws.com:5432','tpwzzptiuhtksa','a7b22a1dfefeb1bb3a52f9a60b86bc05e9699c7ab77240b1581a45e57008a329','d37iem32tm4a')or die("Could not connect to mysql".mysqli_error($con));
 
 ?>
